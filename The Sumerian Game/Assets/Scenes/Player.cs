7c412abe96dffedc9bce6535635452f91935fb65
Alexander <markhamonca@codeninjas.com>
2023-06-07 18:50:15 -0400

    Completed The Sumerian Game

--- The Sumerian Game/Assets/Scenes/Player.cs
+++ The Sumerian Game/Assets/Scenes/Player.cs
@@ -13,16 +13,23 @@ public class Player : MonoBehaviour
     public TMP_Text yearText;
     public TMP_Text highestPopText;
 
-
     // Start is called before the first frame update
     void Start()
     {
         nameText.text = "Ruler: " + playerName;
+
+        gameManager = GetComponent<GameplayManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        yearText.text = "Year: " + gameManager.year;
+
+        if (gameManager.population > highestPop) 
+        {
+            highestPop = gameManager.population;
+            highestPopText.text = "Highest Population: " + highestPop;
+        }
     }
 }

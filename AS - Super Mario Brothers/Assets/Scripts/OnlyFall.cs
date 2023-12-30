388420acb80a0dd9211b7458784d0cd705bca638
CNMarkham <markhamonca@codeninjas.com>
2023-12-30 13:59:58 -0500

    step 130

--- AS - Super Mario Brothers/Assets/Scripts/OnlyFall.cs
+++ AS - Super Mario Brothers/Assets/Scripts/OnlyFall.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Only : MonoBehaviour
+public class OnlyFall : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 velocity = rb.velocity;
+        velocity.y = Mathf.Min(velocity.y, 0);
+        rb.velocity = velocity;
     }
 }

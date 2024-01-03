05419d061c2b972362e4054f24d541f00974ba7f
CNMarkham <markhamonca@codeninjas.com>
2024-01-02 19:45:11 -0500

    Step 155

---
+++ AS - Super Mario Brothers/Assets/Scripts/DeathBarrier.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerBehavior>().Hit();
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}

9c64c8cdec4c5b0dc9791dd2da9a2fcd8a27eed1
Griffin <markhamonca@codeninjas.com>
2024-01-25 19:40:44 -0500

    Completed Purple Belt!!!!!

--- AS - Tetris/Assets/Scripts/Tetromino.cs
+++ AS - Tetris/Assets/Scripts/Tetromino.cs
@@ -7,19 +7,88 @@ public class Tetromino : MonoBehaviour
     public float speed;
     private float previousTime;
     public float fallTime;
-    public float width;
-    public float height;
+    public static int width = 10;
+    public static int height = 20;
     public Vector3 rotationPoint;
+
+
+    public static Transform[,] grid = new Transform[width, height];
+
+    public void CheckForLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    public bool HasLine(int i)
+    {
+        for (int j=0; j < width; j++)
+        {
+            if (grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    public void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    public void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, y] != null)
+                {
+                    grid[x, y - 1] = grid[x, y];
+                    grid[x, y] = null;
+                    grid[x, y - 1].transform.position += Vector3.down;
+                }
+            }
+        }
+    }
+
+    public void AddToGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+
+            grid[x, y] = child;
+        }
+
+    }
+
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckForLines();
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             Vector3 convertedPoint = transform.TransformPoint(rotationPoint);
             transform.RotateAround(convertedPoint, Vector3.forward, 90);
+
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
@@ -30,12 +99,20 @@ public class Tetromino : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.position = transform.position + Vector3.left * speed * Time.deltaTime;
+            transform.position = transform.position + Vector3.left;
+            if (!ValidMove())
+            {
+                transform.position += Vector3.right;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = transform.position + Vector3.right * speed * Time.deltaTime;
+            transform.position = transform.position + Vector3.right;
+            if (!ValidMove())
+            {
+                transform.position += Vector3.left;
+            }
         }
 
         float tempTime = fallTime;
@@ -49,6 +126,14 @@ public class Tetromino : MonoBehaviour
         {
             transform.position = transform.position + Vector3.down;
             previousTime = Time.time;
+            if (!ValidMove())
+            {
+                
+                transform.position += Vector3.up;
+                this.enabled = false;
+                AddToGrid();
+                FindObjectOfType<Spawner>().SpawnTetromino();
+            }
         }
     }
 
@@ -67,9 +152,14 @@ public class Tetromino : MonoBehaviour
             {
                 return false;
             } 
+            if (grid[x, y] != null)
+            {
+                return false;
+            }
         }
         return true;
 
     }
     
 }
+ 
